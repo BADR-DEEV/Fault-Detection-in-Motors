162e002bd654446ef90801d7f50071bb4a4a99cf
BADR-DEEV <bsh20351badersh20002@gmail.com>
2026-07-06 02:04:00 +0200

    updated structure

--- src/utils/load_trained_model.py
+++ src/utils/load_trained_model.py
@@ -2,7 +2,7 @@
 
 import torch
 
-from notebooks.train_mul_cnn import MultiChannelCNN
+from playground.notebooks.train_mul_cnn import MultiChannelCNN
 def load_trained_model(path_to_pth):
     checkpoint = torch.load(path_to_pth)
     
